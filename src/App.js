4323df1d42164cff4fdc21ab3561d2b632002445
prina <prina.gudhka@somaiya.edu>
2022-01-29 18:42:03 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { ToastContainer } from "react-toastify";
+import http from './services/httpService';
+import config from './config.json';
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    const { data: posts } = await http.get(config.apiEndpoint);
+    this.setState({ posts });
+  }
+
+  handleAdd = async () => {
+    const obj = { title: "testTitle", body: "lorem ipsum" };
+    const { data: post } = await http.post(config.apiEndpoint, obj);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
+  };
+
+  handleUpdate = async post => {
+    post.title = "UPDATED";
+    await http.put(config.apiEndpoint + '/' + post.id,post);
+    // await http.put(`${config.apiEndpoint}/${post.id}`, post);
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(post);
+    posts[index] = { ...post };
+    this.setState({ posts });
+  };
+
+  handleDelete = async post => {
+    const originalPosts = this.state.posts;
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({ posts });
+
+      try {
+        await http.delete(config.apiEndpoint + "/" + post.id);
+      } catch (ex) {
+        if (ex.response && ex.response.status === 404)
+            alert("This post has already been deleted");
+      
+          this.setState({ posts: originalPosts });
+      }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+  
 }
 
 export default App;
